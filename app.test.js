ea9d2f80c533b0612efc67f09f33ebbcee12cb71
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-07-06 14:18:27 +0200

    make the third test

--- app.test.js
+++ app.test.js
@@ -23,6 +23,14 @@ it('should return -21 if first array is [-1,-2,-3] and second is [-4,-5,-6]', ()
     expect(result).toBeTypeOf("number");
 })
 
+it('should return 15 if first array is [0,0,0] and second is [4,5,6]', () => {
+    const firstAray = [0,0,0];
+    const secondAray = [4,5,6];
+    const result= arraysAdition(firstAray, secondAray);
+
+    expect(result).toBe(15);
+    expect(result).toBeTypeOf("number");
+})
 
 
 })
